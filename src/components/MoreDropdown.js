cb1ee386b6c1f7c3fca5d4ba5250597176eb5a6d
BWNC-code <>
2023-11-04 16:20:57 +0000

    add react dropdown menu for user post changes

---
+++ src/components/MoreDropdown.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+import styles from "../styles/MoreDropdown.module.css";
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+  <i
+    className="fas fa-ellipsis-v"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const MoreDropdown = () => {
+  return (
+    <Dropdown className="ml-auto" drop="down">
+      <Dropdown.Toggle as={ThreeDots} id="dropdown-custom-components">
+        Custom toggle
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className="text-center">
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={() => {}}
+          aria-label="edit"
+        >
+          <i className="fas fa-edit" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={() => {}}
+          aria-label="delete"
+        >
+          <i className="fas fa-trash" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
