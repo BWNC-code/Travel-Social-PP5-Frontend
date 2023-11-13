98179aad9a36c9fc38a27d597dbc1d84c4cb7599
BWNC-code <>
2023-11-13 17:31:41 +0000

    add link back to homepage

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NoResults from "../assets/no-results.png";
 import styles from "../styles/NotFound.module.css";
 import Asset from "./Asset";
 
 const NotFound = () => {
   return (
-    <div className={styles.margin_top}>
+    <div className={`${styles.margin_top} text-center`}>
       <Asset
         src={NoResults}
         message="Sorry, the page you're looking for doesn't exist."
       />
+      <Link  to={"/"}>Click here to return to the Homepage.</Link>
     </div>
   );
 };
