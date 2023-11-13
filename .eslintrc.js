52826144276d244207255bd7679cf86020dff749
BWNC-code <>
2023-11-13 18:10:25 +0000

    fix rules and set env values

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/display-name": "off",
+  },
+};
